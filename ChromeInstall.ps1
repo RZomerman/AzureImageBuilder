0c568fde24480a72b38534d7aaa0487c1fde9f96
Roelf Zomerman <RZomerman@users.noreply.github.com>
2020-09-16 19:42:01 +0400

    Update ChromeInstall.ps1

--- ChromeInstall.ps1
+++ ChromeInstall.ps1
@@ -1,5 +1,36 @@
 #ChromeInstall.ps1
+
 $workfolder="c:\deployment"
+$uri1='https://dl.google.com/dl/chrome/install/GoogleChromeEnterpriseBundle64.zip'
+$FileName="GoogleChromeEnterpriseBundle64.zip"
+
+
+
+If(!(test-path $workfolder)){
+    new-item -Path c:\ -Name Deployment -ItemType Directory
+}
+
+If (!(test-path C:\deployment\BuildCustomImage.psm1)){
+    Invoke-webrequest -uri https://raw.githubusercontent.com/RZomerman/AzureImageBuilder/master/BuildCustomImage.psm1 -OutFile C:\deployment\BuildCustomImage.psm1 -UseBasicParsing
+}
+If (!(Get-Module -Name BuildCustomImage )){
+    Import-Module C:\deployment\BuildCustomImage.psm1
+}
+
+
+
+##Setting Global Paramaters##
+$ErrorActionPreference = "Stop"
+$date = Get-Date -UFormat "%Y-%m-%d-%H-%M"
+#$workfolder = Split-Path $script:MyInvocation.MyCommand.Path
+$workfolder = "C:\Deployment"
+$logFile = $workfolder+'\GoogleChrome'+$date+'.log'
+WriteLog -Message "Steps will be tracked on the log file : [ $logFile ]" -LogFile $logfile
+
+Writelog -Message "Downloading files" -LogFile $logfile
+DownloadWithRetry -url $URI1 -downloadLocation ($workfolder + "\" + $FileName) -retries 3 -LogFile $logfile
+
+
 $ChromeZipFile=Get-ChildItem -Path $workfolder -Filter GoogleChrome*.zip -Recurse -File -Name  | ForEach-Object {
     $ZIPFolderName=[System.IO.Path]::GetFileNameWithoutExtension($_)
     Expand-Archive ($workfolder + "\" + $_) -DestinationPath ($workfolder + "\" + $ZIPFolderName) -Force}
