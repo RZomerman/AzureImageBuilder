6a2176ba70d31961581cb84dd95529ba38630297
Roelf Zomerman <RZomerman@users.noreply.github.com>
2020-09-16 18:46:20 +0400

    Update VSCode.ps1

--- VSCode.ps1
+++ VSCode.ps1
@@ -1,12 +1,34 @@
 #VSCodeInstall.ps1
 $workfolder="c:\deployment"
+$uri1='https://go.microsoft.com/fwlink/?Linkid=852157'
+$FileName="VSCodeSetup.exe"
+
+
+If(!(test-path $workfolder)){
+    new-item -Path c:\ -Name Deployment -ItemType Directory
+}
+
+If (!(test-path C:\deployment\BuildCustomImage.psm1)){
+    Invoke-webrequest -uri https://raw.githubusercontent.com/RZomerman/AzureImageBuilder/master/BuildCustomImage.psm1 -OutFile C:\deployment\BuildCustomImage.psm1 -UseBasicParsing
+}
+If (!(Get-Module -Name BuildCustomImage )){
+    Import-Module C:\deployment\BuildCustomImage.psm1
+}
+
+
+##Setting Global Paramaters##
+$ErrorActionPreference = "Stop"
+$date = Get-Date -UFormat "%Y-%m-%d-%H-%M"
+$logFile = $workfolder+'\VSCode'+$date+'.log'
+WriteLog -Message "Steps will be tracked on the log file : [ $logFile ]" -Logfile $logfile
+
+Writelog -Message "Downloading files" -Logfile $logfile
+DownloadWithRetry -url $URI1 -downloadLocation ($workfolder + "\" + $FileName) -retries 3 -LogFile $logfile
+WriteLog -Message "Download complete" -Logfile $logfile
+
+
 $VSCode=Get-ChildItem -Path $workfolder -Filter VSCodeSetup*.exe -Recurse -File -Name 
     $File=($workfolder + "\" + $VSCode)
-    #$Command=("$File /VERYSILENT /MERGETASKS=!runcode")
-    #&"$File"
-
+    WriteLog -Message "Staring install of $VSCode" -Logfile $logfile
     Start-Process -FilePath $File -Argument "/VERYSILENT /MERGETASKS=!runcode" -Wait
-
-
-
-
+    WriteLog -Message "Finished install" -Logfile $logfile
